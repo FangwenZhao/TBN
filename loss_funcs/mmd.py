926a9ced89f2ffa769e9b078d4fb82c3929b643a
dbsdzxq <dbsdzxq@outlook.com>
2022-08-16 14:40:16 +0800

    update

--- loss_funcs/mmd.py
+++ loss_funcs/mmd.py
@@ -10,26 +10,6 @@ class MMDLoss(nn.Module):
         self.fix_sigma = None
         self.kernel_type = kernel_type
 
-    def guassian_kernel_(self, source, target, kernel_mul, kernel_num, fix_sigma):
-        n_samples = int(source.size()[0]) + int(target.size()[0])
-        total = torch.cat([source, target], dim=0)
-        total0 = total.unsqueeze(0).expand(
-            int(total.size(0)), int(total.size(0)), int(total.size(1)))
-        total1 = total.unsqueeze(1).expand(
-            int(total.size(0)), int(total.size(0)), int(total.size(1)))
-        L2_distance = ((total0-total1)**2).sum(2)
-
-        if fix_sigma:
-            bandwidth = fix_sigma
-        else:
-            bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
-        bandwidth /= kernel_mul ** (kernel_num // 2)
-        bandwidth_list = [bandwidth * (kernel_mul**i)
-                          for i in range(kernel_num)]
-        kernel_val = [torch.exp(-L2_distance / bandwidth_temp)
-                      for bandwidth_temp in bandwidth_list]
-        return sum(kernel_val)
-
 
     def guassian_kernel(self, source, target, kernel_mul, kernel_num, fix_sigma):
         n_samples = int(source.size()[0]) + int(target.size()[0])
